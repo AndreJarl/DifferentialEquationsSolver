0343041846170b1f9ad1225224c6d63d3337b954
Andre Jarl <andrejarlaniana@gmail.com>
2023-10-07 22:50:11 +0800

    fixed hero responsiveness

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,14 @@
+import {AiOutlineMenu} from 'react-icons/ai'
 
 function Navbar(){
   
     return(
         <>
-        <div className="flex justify-between p-6 bg-red-500 text-white">
+        <div className=" flex items-center justify-between p-6 bg-red-500 text-white z-50">
             <h1 className="text-2xl font-semibold">DECalc</h1>
-            <p>---</p>
+            <h1 className='text-4xl'><AiOutlineMenu /></h1>
             <div className="sm:absolute sm:hidden sm:right-0 sm:top-0 bg-red-500 w-1/2 h-full p-4 flex  " >
-                <p>X</p>
+               
                 <ul className="sm:flex-col sm:gap-10 sm:mb-10 sm:text-2xl sm:text-center sm:ml-5 text-white font-medium sm:pt-16">
                     <a href=""><li className="mb-10">Home</li></a>
                     <a href=""><li className="mb-10">Calculate</li></a>
