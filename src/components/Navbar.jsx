ef043bf5243f82e1bab724cf25622536eb18ca0e
Andre Jarl <andrejarlaniana@gmail.com>
2023-10-08 18:43:20 +0800

    change navbar bg

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,16 +6,16 @@ function Navbar(){
     return(
         <>
      <div className='fixed w-full pt-4 m-0 sm:pt-0'>
-        <div className="flex  items-center justify-between p-6 h-24 bg-red-700 text-white">
+        <div className="flex  items-center justify-between p-6 h-24 bg-white text-red-600">
             <h1 className="text-4xl font-bold pl-10">DECalc</h1>
             <h1 className='text-4xl lg:hidden sm:block'><AiOutlineMenu /></h1>
             <div className="pt-9 pr-10 sm:hidden lg:block">
                
                 <ul className="lg:flex lg:gap-20 items-center text-lg font-semibold">
-                    <a href=""><li className="mb-10">Home</li></a>
-                    <a href=""><li className="mb-10">Calculate</li></a>
-                    <a href=""><li className="mb-10">About</li></a>
-                    <a href=""><li className="mb-10 text-3xl"><AiOutlineQuestionCircle/></li></a>
+                    <a href=""><li className="mb-10 hover:underline">Home</li></a>
+                    <a href=""><li className="mb-10 hover:underline">Calculate</li></a>
+                    <a href=""><li className="mb-10 hover:underline">About</li></a>
+                    <a href=""><li className="mb-10 text-3xl hover:text-red-400"><AiOutlineQuestionCircle/></li></a>
 
                 </ul>
             </div>
