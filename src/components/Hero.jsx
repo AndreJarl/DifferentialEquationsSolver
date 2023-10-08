971c334711ed1595d9b27e693ce4856727a7718d
Andre Jarl <andrejarlaniana@gmail.com>
2023-10-08 09:33:33 +0800

    minor herp adjustments

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,14 +3,14 @@ import hero from "../assets/hero.png"
 function Hero(){
     return (
         <>
-        <div className="lg:-mt-4 sm:mt-16 mx-10 flex justify-center items-center">
+        <div className="lg:-mt-0 sm:pt-40 lg:pt-20 mx-10 flex justify-center items-center ">
             <div className="flex justify-center items-center gap-4 flex-row sm:flex-col mb-5">
                 <div className="flex gap-5 lg:flex-row sm:flex-col justify-center items-center">
-                  <div className="flex sm:flex-col justify-center items-center gap-3">
+                  <div className="flex sm:flex-col justify-center items-center gap-3 lg:-mt-14">
                         <p className="text-center sm:text-5xl font-bold text-red-600 sm:m-5">DIFFERENTIAL EQUATIONS</p>
-                        <p className="text-center lg:mx-9 sm:mx-4 sm:pb-2 text-lg font-semibold text-gray-500">Classications, Elimination of Arbitrary Constants, Application of Seperable Variable and many more.....</p>
+                        <p className="text-center lg:mx-14 sm:mx-4 sm:pb-2 text-lg font-semibold text-gray-500">Classifications, Elimination of Arbitrary Constants, Application of Seperable Variable and many more.....</p>
                         <p className="text-center sm:text-lg font-semibold">Group 3 Final Project</p>
-                        <button className="bg-red-600 px-11 py-2 mt-2 text-xl text-white font-semibold rounded-xl">Solve Equations ></button>
+                        <button className="bg-red-600 hover:bg-red-400 px-11 py-2 mt-2 text-xl text-white font-semibold rounded-xl">Solve Equations ></button>
                     </div>
                    <img className="sm:w-96 lg:w-[500px] "  src={hero} alt="" srcset="" />
                    
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,26 @@
-import {AiOutlineMenu} from 'react-icons/ai'
+import {AiOutlineMenu , AiOutlineQuestionCircle} from 'react-icons/ai'
+import { useState } from 'react'
 
 function Navbar(){
   
     return(
         <>
-        <div className=" flex items-center justify-between p-6 bg-red-500 text-white z-50">
-            <h1 className="text-2xl font-semibold">DECalc</h1>
-            <h1 className='text-4xl'><AiOutlineMenu /></h1>
-            <div className="sm:absolute sm:hidden sm:right-0 sm:top-0 bg-red-500 w-1/2 h-full p-4 flex  " >
+     <div className='fixed w-full pt-4 m-0 sm:pt-0'>
+        <div className="flex  items-center justify-between p-6 h-24 bg-red-700 text-white">
+            <h1 className="text-4xl font-bold pl-10">DECalc</h1>
+            <h1 className='text-4xl lg:hidden sm:block'><AiOutlineMenu /></h1>
+            <div className="pt-9 pr-10 sm:hidden lg:block">
                
-                <ul className="sm:flex-col sm:gap-10 sm:mb-10 sm:text-2xl sm:text-center sm:ml-5 text-white font-medium sm:pt-16">
+                <ul className="lg:flex lg:gap-20 items-center text-lg font-semibold">
                     <a href=""><li className="mb-10">Home</li></a>
                     <a href=""><li className="mb-10">Calculate</li></a>
                     <a href=""><li className="mb-10">About</li></a>
-                    <a href=""><li className="mb-10">?</li></a>
+                    <a href=""><li className="mb-10 text-3xl"><AiOutlineQuestionCircle/></li></a>
 
                 </ul>
             </div>
         </div>
+     </div>
         </>
     )
 
