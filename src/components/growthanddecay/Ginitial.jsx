d7773007335ac6e561d584bd9be12e5c1394a512
Andre Jarl <andrejarlaniana@gmail.com>
2023-12-09 19:20:45 +0800

    added solution in Ginitial and Gpopulation

--- src/components/growthanddecay/Ginitial.jsx
+++ src/components/growthanddecay/Ginitial.jsx
@@ -6,6 +6,8 @@ const Ginitial = () => {
   const [time1, setTime1] = useState('');
   const [time2, setTime2] = useState('');
   const [initialPopulation, setInitialPopulation] = useState(null);
+  const [showSolution, setShowSolution] = useState(false); // New state
+  const [k, setK] = useState(null);
 
   const handlePopulationAtTime1Change = (e) => {
     setPopulationAtTime1(e.target.value);
@@ -27,6 +29,8 @@ const Ginitial = () => {
     // Validate the inputs
     if (!populationAtTime1 || !populationAtTime2 || !time1 || !time2 || isNaN(parseFloat(populationAtTime1)) || isNaN(parseFloat(populationAtTime2)) || isNaN(parseFloat(time1)) || isNaN(parseFloat(time2))) {
       alert('Please enter valid values for populations and times.');
+      setShowSolution(false); // Set showSolution to true when the button is clicked
+
       return;
     }
 
@@ -41,8 +45,10 @@ const Ginitial = () => {
 
     // Calculate initial population (P0)
     const calculatedInitialPopulation = parsedPopulationAtTime1 / Math.exp(parsedTime1 * k);
-
+    setK(k.toFixed(6));
     setInitialPopulation(calculatedInitialPopulation.toFixed(2));
+    setShowSolution(true); // Set showSolution to true when the button is clicked
+
   };
 
   return (
@@ -101,7 +107,30 @@ const Ginitial = () => {
       </div>
       <button className='bg-red-600 px-5 py-1 flex justify-center items-center text-white font-semibold rounded-md text-xl'  onClick={calculateInitialPopulation}>Calculate Initial Value</button>
       <div className='bg-red-600 text-white min-w-[350px] md:min-w-[600px] lg:min-w-[600px] max-w-full text-center py-5 mb-10 text-xl font-bold flex flex-col justify-center gap-4 '> 
+      <div className={`solution ${showSolution ? 'visible' : 'hidden'}`}> 
+        <div className='flex flex-col justify-center gap-10 pb-10 pt-10 sm:text-lg'>
+           <div className='flex gap-10  justify-center'>
+                     <p>Given: </p>
+                     <p> {`@t = ${time1}, P = ${populationAtTime1}`} <br/>
+                     {`@t = ${time2}, P = ${populationAtTime2}`}
+                     </p>
+               </div>
+            <div className='flex gap-10  justify-center'>
+                <p>Req'd: </p>
+                <p>{`@t = 0, P = ?`}
+                </p>
+              </div>  
+           <div className='flex flex-col gap-2  justify-center'>
+                <p className='text-left ml-28'>Sol'n: </p>
+                 <p>{`P/P = P0e^k(t)/P0e^k(t), k = ? `}<br/>
+                    {`P0 = P/e^t(k), Po = ?`}
+                 </p>
+              </div>  
+          </div>
+          <p>k = {k}</p>
+      </div>
       {initialPopulation !== null && (
+        
         <p>Initial Value: {initialPopulation}</p>
       )}
     </div>
--- src/components/growthanddecay/Population.jsx
+++ src/components/growthanddecay/Population.jsx
@@ -1,3 +1,4 @@
+import { composition } from 'mathjs';
 import React, { useState } from 'react';
 
 function SeparableVariable() {
@@ -8,6 +9,7 @@ function SeparableVariable() {
   const [result, setResult] = useState(null);
   const [k, setK] = useState(null);
   const [dp, setDp] = useState(null);
+  const [showSolution, setShowSolution] = useState(false); // New state
 
   const solveEquation = () => {
     const initial = parseFloat(P0);
@@ -20,12 +22,16 @@ function SeparableVariable() {
       const finalValue =  initial * Math.exp(constant * time2);
       const dpdt = initial * constant * Math.exp(constant * time2); 
       setResult(`Population: ${finalValue.toFixed(3)}`);
-      setK(`Rate Constant (k): ${constant.toFixed(20)}`);
+      setK(`Rate Constant (k): ${constant.toFixed(6)}`);
       setDp(`dP/dt: ${dpdt.toFixed(3)} per year`);
+      setShowSolution(true); // Set showSolution to true when the button is clicked
+
     } else {
       setResult('Invalid Input');
       setK('Invalid Input');
       setDp('Invalid Input');
+      setShowSolution(false); // Set showSolution to true when the button is clicked
+
     }
   };
 
@@ -84,19 +90,44 @@ function SeparableVariable() {
       </div>
      </div>
       <button className='bg-red-600 px-5 py-1 flex justify-center items-center text-white font-semibold rounded-md text-xl' onClick={solveEquation}>Solve</button>
-      <div className='bg-red-600 text-white min-w-[350px] md:min-w-[600px] lg:min-w-[600px] max-w-full text-center py-5 mb-10 text-xl font-bold flex flex-col justify-center gap-4 '> 
-    
+      <div className='rounded-xl bg-red-600 text-white min-w-[350px] md:min-w-[600px] lg:min-w-[600px] max-w-full text-center py-5 mb-10 text-xl font-bold flex flex-col justify-center gap-4 '> 
+      
+      <div className={`solution ${showSolution ? 'visible' : 'hidden'}`}> 
+         <div className='flex flex-col justify-center gap-10 pb-10 pt-10 sm:text-lg'>
+              <div className='flex gap-10  justify-center'>
+                <p>Given: </p>
+                <p>{`@t = 0,  P= ${P0}`} <br></br>
+                   {`@t = ${t}, P = ${pt}`}
+                </p>
+              </div>
+              <div className='flex gap-10  justify-center'>
+                <p>Req'd: </p>
+                <p>{`@t = ${t2}, P = ?`}
+                </p>
+              </div>
+              <div className='flex gap-10  justify-center'>
+                <p>Sol'n: </p>
+                <p>{`@t = ${t}, P = ${pt}`} <br />
+                   {` P = ce^kt `}<br/>
+                {`${pt} = ${P0}e^k(${t}), k = ?`}<br/>
+                {`P = ${P0}e^k(${t2}), P = ?`}
+                </p>
+              </div>
+         </div>
+       </div> 
+
         <div>
           {k && <p>{k}</p>}
         </div>
 
         <div>
-          {result && <p> {result}</p>}
+          {result && <p> {result} </p>} 
+          
         </div>
 
-        <div>
+        {/* <div>
           {dp && <p> {dp}</p>}
-        </div>
+        </div> */}
 
       </div>
     </div>
@@ -105,3 +136,10 @@ function SeparableVariable() {
 
 export default SeparableVariable;
 
+
+// {`==> Given: @t = 0,  P= ${P0}`} <br></br>
+// {`@t = ${t}, P = ${pt}`}<br></br>
+// {`==> Req'd: @t = ${t2}, P = ?`}<br></br>
+// {`==> Sol'n: P = ce^kt `} <br></br>
+// {`@t = ${t}, P = ${pt}`}<br></br>
+// {`${pt} = ${P0}e^k(${t})`}
