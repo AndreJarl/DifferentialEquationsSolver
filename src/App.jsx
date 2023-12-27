7a5c629035e40b4986b8e3f466e97b13bbc8c5a7
Andre Jarl <andrejarlaniana@gmail.com>
2023-12-27 14:45:32 +0800

    updated routers

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import Home from './Home';
 import About from './routes/About';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <Router>
+    
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
       </Routes>
-    </Router>
+    
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+import './index.css';
 import { HashRouter } from 'react-router-dom';
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.render(
   <React.StrictMode>
     <HashRouter>
-     <App/>
-     </HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>,
-)
+  document.getElementById('root')
+);
