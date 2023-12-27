e36eb1f07db41f8a3d0676796231a382b2d82a94
Andre Jarl <andrejarlaniana@gmail.com>
2023-12-27 15:12:35 +0800

    change router

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
-import { AiOutlineMenu, AiOutlineQuestionCircle } from 'react-icons/ai';
 import { useState } from 'react';
-import { Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
+
 function Navbar() {
   const [showSolveMenu, setShowSolveMenu] = useState(false);
 
@@ -17,12 +17,15 @@ function Navbar() {
       <div className='bg-red-500 fixed w-screen'>
         <div className='bg-red-500 flex gap-4 mx-5 my-10 items-center justify-between lg:gap-60 lg:mx-36 lg:my-5 md:flex md:gap-10 md:mx-32'>
           <div className=''>
-          <a href="#hero"> <Link to="/"><p className='text-4xl font-bold text-white cursor-pointer lg:text-5xl'>DESolver</p></Link></a>
+            
+              <Link to="/"><p className='text-4xl font-bold text-white cursor-pointer lg:text-5xl'>DESolver</p></Link>
+             
           </div>
+          
           <div>
             <ul className='flex gap-10 list-none font-bold text-white text-lg lg:gap-28 lg:text-xl'>
               <div onClick={handleSolveClick}>
-                <Link to="/"><li className='hover:text-black hover:cursor-pointer'>Solve</li></Link>
+              <li className='hover:text-black hover:cursor-pointer'>Solve</li>
                 {showSolveMenu && (
                   <div className='absolute bg-red-200 h-70 text-base px-5 justify-center items-center pb-10 -ml-28 rounded-xl mt-6 text-red-800'>
                     <a href="#class"><li className='pt-10' onClick={handleMenuItemClick}>Classification</li></a>
@@ -31,7 +34,7 @@ function Navbar() {
                   </div>
                 )}
               </div>
-              <Link to="/about">  <li className='hover:text-black hover:cursor-pointer'>About</li></Link>
+              <Link to="/about"><li className='hover:text-black hover:cursor-pointer'>About</li></Link>
             </ul>
           </div>
         </div>
